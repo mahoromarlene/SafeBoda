3235145e7f8dc4ed9067545d143352e8259b75e3
Marlene Uwamahoro <uwamahoromarlene@gmail.com>
2017-06-16 14:41:27 +0200

    Created a signup page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get "/login", to: 'pages#login'
+  get "/signup", to: 'pages#signup'
 end
